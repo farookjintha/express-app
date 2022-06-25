66ac92fce3d4ce60554b731cad2ad47e22e551da
Farook Jintha <farookjintha@gmail.com>
2022-06-25 12:34:56 +0530

    minor changes

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.use('/backend', (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>{
-    console.log(`Listening on Port ${port}..!`);
+    console.log(`Listening on Port ${PORT}..!`);
 });
